4df05dc8c1974c40ca978c1089b751860bffbf5a
Di <d.osseyran@hotmail.com>
2022-08-18 21:24:33 +0200

    here you will have extra pages created xD

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,4 +5,18 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get("/about",(req,res,next)=>{
+  res.render("about")
+})
+router.get("/ListOfBooks",(req,res,next)=>{
+  res.render("ListOfBooks")
+})
+
+// controller! 
+// "/contact-us" is called slash contact-us end point. 
+// a controller (ex: get, post, delete etc) is a way to handle requests. 
+router.get("/contact-us", (req,res,next)=>{
+  res.render("contact-us")
+})
+
 module.exports = router;
--- utils/consts.js
+++ utils/consts.js
@@ -1,3 +1,4 @@
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/AwesomeBooksLending";
-
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/AwesomeBooksLending";
+//if it doesn't exist we are giving it the second value, which is our local database (which is a string)
 module.exports = MONGO_URI;
