278824fb914aa1040219f6f96c334298fa559235
Di <d.osseyran@hotmail.com>
2022-08-20 17:03:05 +0200

    Models corrected by Filipe and André

--- db/book.model.js
+++ db/book.model.js
@@ -1,27 +1,32 @@
 const mongoose = require("mongoose");
-const BooksSchema = new mongoose.Schema({
-    title: {
-        type: [String],
-        unique: true,
-        required: true,
-    }
-    author: {
-        type: String,
-        unique: true,
-        required: true,
-    }
-    publisher: {
-        type: String,
-        required: true,
-    },
-    nsfw:{
-        type: Boolean,
-        default: false,
-    },
-
-
-
-
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  author: {
+    type: [String],
+    required: true,
+  },
+  publisher: {
+    type: String,
+    required: true,
+  },
+  //nsfw(not safe for words): {
+  // type: Boolean,
+  //default: false,
+  //},
+  description: {
+    type: String,
+  },
+  year: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    // we can add a "default" value to avoid screwing the page design
+  },
 });
 const BookModel = mongoose.model("books", BookSchema);
 module.exports = BookModel;
--- db/user.model.js
+++ db/user.model.js
@@ -1,20 +1,30 @@
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-    }
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-    }
-    password: {
-        type: String,
-        required: true,
+  // username: {
+  //   type: String,
+  // unique: true,
+  // required: true,
+  // },
 
-    },
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 const UserModel = mongoose.model("user", UserSchema);
 module.exports = UserModel;
