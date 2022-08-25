bdbba611cd6c9deacdfbf3cce6420ab361a4b8e8
Di <d.osseyran@hotmail.com>
2022-08-25 19:23:02 +0200

    updating the project

--- db/book.model.js
+++ db/book.model.js
@@ -9,6 +9,7 @@ const BookSchema = new mongoose.Schema({
     type: [String],
     required: true,
   },
+  //emum is a schema validator . We can use an emum to save creating genre model.
   publisher: {
     type: String,
     required: true,
@@ -27,6 +28,13 @@ const BookSchema = new mongoose.Schema({
     type: String,
     // we can add a "default" value to avoid screwing the page design
   },
+  genre: {
+    type: [String],
+    enum: ["history", "comedy", "romance", "adventure", "psychology"],
+  },
 });
+
+//just like in the recipe lab the skill level is the genre here
+
 const BookModel = mongoose.model("books", BookSchema);
 module.exports = BookModel;
